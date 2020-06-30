bf943dd8d49edbd288052748c08393381a50ffed
filipe-nasci <filipevaladao19@gmail.com>
2020-06-29 22:15:27 -0300

    saiba mais sobre os herois da marvel e DC

---
+++ herois.js
@@ -0,0 +1,9 @@
+const rs = require("readline-sync")
+const axios = require("axios")
+const fs = require("fs")
+
+var idheroi = rs.question("Digite um numero de 1 a 731): ").toLocaleLowerCase()
+
+axios.get(`https://superheroapi.com/api/10217487131151138/${idheroi}`) 
+.then((resposta) => {
+    console.log(resposta.data)})
